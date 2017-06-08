89c2058def0fa316670b1119c4556ddd65d4d69a
Jonathan Cammisuli <jon@cammisuli.ca>
2017-06-08 08:54:34 -0400

    clean up component and unused files

--- src/component.ts
+++ src/component.ts
@@ -12,52 +12,9 @@ export function CustomElement<T extends HTMLElementConstructor>(elementName: str
             }
         }
 
-        //register element
+        // register element
         window.customElements.define(elementName, customElementClass);
 
         return customElementClass;
     }
 }
-
-// import { h, render } from 'preact';
-
-
-// type Props<T> = {
-//     [K in keyof T]: T[K];
-// }
-
-// export abstract class CustomComponent<T> extends HTMLElement {
-
-//     public abstract props: Props<T>;
-//     private _dom: Element;
-
-//     constructor() {
-//         super();
-//         if (!this.shadowRoot) {
-//             this.attachShadow({ mode: 'open' });
-//         }
-//     }
-
-//     connectedCallback() {
-//         // Sync properties to attributes
-//         this._renderTemplate();
-//     }
-
-//     disconnectedCallback() {
-
-//     }
-
-//     attributeChangedCallback(attributeName: string, oldValue: any, newValue: any) {
-
-//     }
-
-//     private _renderTemplate() {
-//         this._dom = render(this.template(this.props), <any>this.shadowRoot, this._dom);
-//     }
-
-//     abstract template(props?: T): JSX.Element;
-
-// }
-
-// export { h } from 'preact';
-
